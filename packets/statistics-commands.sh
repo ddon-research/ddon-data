65ce07acd0bddc5a141c7010987cfb2f541e4b9e
Sehkah <sehkah.vhemrada@gmail.com>
2024-04-28 21:25:26 +0200

    Update statistics for unknown packets

--- packets/statistics-commands.sh
+++ packets/statistics-commands.sh
@@ -2,13 +2,16 @@ grep -h -e "C2S" * | perl -p -e "s/Server|Client //g;" -e "s/Pcap.*//g;" -e "s/#
 grep -h -e "S2C" * | perl -p -e "s/Server|Client //g;" -e "s/Pcap.*//g;" -e "s/#\d+//g;" -e "s/\s+/ /" | sort | uniq > ../decrypted-tcp-streams-unique-s2c-packets.txt
 grep -h -e "L2C" * | perl -p -e "s/Server|Client //g;" -e "s/Pcap.*//g;" -e "s/#\d+//g;" -e "s/\s+/ /" | sort | uniq > ../decrypted-tcp-streams-unique-l2c-packets.txt
 grep -h -e "C2L" * | perl -p -e "s/Server|Client //g;" -e "s/Pcap.*//g;" -e "s/#\d+//g;" -e "s/\s+/ /" | sort | uniq > ../decrypted-tcp-streams-unique-c2l-packets.txt
+grep -h -e "Unknown Pcap" * | perl -p -e "s/Server|Client //g;" -e "s/Pcap.*//g;" -e "s/#\d+//g;" -e "s/\s+/ /" | sort | uniq > ../decrypted-tcp-streams-unique-unknown-packets.txt
 
 while read h; do count=$(grep -h -e "$h" * | wc -l); echo $h,$count; done < ../decrypted-tcp-streams-unique-c2s-packets.txt > ../decrypted-tcp-streams-unique-c2s-packets-count.txt
 while read h; do count=$(grep -h -e "$h" * | wc -l); echo $h,$count; done < ../decrypted-tcp-streams-unique-s2c-packets.txt > ../decrypted-tcp-streams-unique-s2c-packets-count.txt
 while read h; do count=$(grep -h -e "$h" * | wc -l); echo $h,$count; done < ../decrypted-tcp-streams-unique-l2c-packets.txt > ../decrypted-tcp-streams-unique-l2c-packets-count.txt
 while read h; do count=$(grep -h -e "$h" * | wc -l); echo $h,$count; done < ../decrypted-tcp-streams-unique-c2l-packets.txt > ../decrypted-tcp-streams-unique-c2l-packets-count.txt
+while read h; do count=$(grep -h -e "$h" * | wc -l); echo $h,$count; done < ../decrypted-tcp-streams-unique-unknown-packets.txt > ../decrypted-tcp-streams-unique-unknown-packets-count.txt
 
 rm decrypted-tcp-streams-unique-c2s-packets.txt
 rm decrypted-tcp-streams-unique-s2c-packets.txt
 rm decrypted-tcp-streams-unique-l2c-packets.txt
 rm decrypted-tcp-streams-unique-c2l-packets.txt
+rm decrypted-tcp-streams-unique-unknown-packets.txt
